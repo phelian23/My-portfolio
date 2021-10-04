9fca15227514551bc2fd106f771d0a9455d446f3
Oluwafemi <femikola23@gmail.com>
2021-10-04 21:47:35 +0400

    Update script and linters remove

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-const crossMenu = document.querySelector(".close");
-const menuCont = document.querySelector(".nav-items");
+const crossMenu = document.querySelector('.togglebar');
+const menuCont = document.querySelector('.nav-items');
+const crossMenu2 = document.querySelector('.close');
+const navItem = document.querySelectorAll('.show-list');
 
-crossMenu.addEventListener("click", closeMobMenu);
+function openMobMenu() {
+  menuCont.classList.add('menu-open');
+}
+
+crossMenu.addEventListener('click', openMobMenu);
 
 function closeMobMenu() {
-  menuCont.classList.remove("menu-open");
+  menuCont.classList.remove('menu-open');
 }
+
+crossMenu2.addEventListener('click', closeMobMenu);
+
+navItem.forEach((menuItem) => {
+  menuItem.addEventListener('click', closeMobMenu);
+});
